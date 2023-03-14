c92a9bc4c8300aba76ec118071c898f365055959
kye <kye6735@naver.com>
2023-03-14 14:52:59 +0900

    구구단 유식하게

--- 2023 CPP 202/구구단.cpp
+++ 2023 CPP 202/구구단.cpp
@@ -4,15 +4,10 @@ int main() {
 
 	for (int i = 2; i <= 9; i++)
 	{
-		printf("%d X 1 = %d\n", i, i * 1);
-		printf("%d X 2 = %d\n", i, i * 2);
-		printf("%d X 3 = %d\n", i, i * 3);
-		printf("%d X 4 = %d\n", i, i * 4);
-		printf("%d X 5 = %d\n", i, i * 5);
-		printf("%d X 6 = %d\n", i, i * 6);
-		printf("%d X 7 = %d\n", i, i * 7);
-		printf("%d X 8 = %d\n", i, i * 8);
-		printf("%d X 9 = %d\n", i, i * 9);
+		for (int j = 1; j <= 9; j++)
+		{
+			printf("%d X %d= %d\n", i, j, i * j);
+		}
 	}
 
 
