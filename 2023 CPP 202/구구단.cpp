7674a40cd5ea413cdd7288554f3f25bae66ca908
kye <kye6735@naver.com>
2023-03-14 14:42:37 +0900

    구구단 무식하게

---
+++ 2023 CPP 202/구구단.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	printf("2 X 1 = 2\n");
+	printf("2 X 2 = 4\n");
+	printf("2 X 3 = 6\n");
+	printf("2 X 4 = 8\n");
+	printf("2 X 5 = 10\n");
+	printf("2 X 6 = 12\n");
+	printf("2 X 7 = 14\n");
+	printf("2 X 8 = 16\n");
+	printf("2 X 9 = 18\n");
+
+	printf("3 X 1 = 3\n");
+	printf("3 X 2 = 6\n");
+	printf("3 X 3 = 9\n");
+	printf("3 X 4 = 12\n");
+	printf("3 X 5 = 15\n");
+	printf("3 X 6 = 18\n");
+	printf("3 X 7 = 21\n");
+	printf("3 X 8 = 24\n");
+	printf("3 X 9 = 27\n");
+
+	printf("4 X 1 = 4\n");
+	printf("4 X 2 = 8\n");
+	printf("4 X 3 = 12\n");
+	printf("4 X 4 = 16\n");
+	printf("4 X 5 = 20\n");
+	printf("4 X 6 = 24\n");
+	printf("4 X 7 = 28\n");
+	printf("4 X 8 = 32\n");
+	printf("4 X 9 = 36\n");
+
+
+	
+	
+	return 0;
+
+}
