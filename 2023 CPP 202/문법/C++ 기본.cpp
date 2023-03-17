4db60225d23e402d5e9ffa0fa4525d3b2ced1e9e
kye <kye6735@naver.com>
2023-03-17 09:51:37 +0900

    변수 출력하기

--- 2023 CPP 202/문법/C++ 기본.cpp
+++ 2023 CPP 202/문법/C++ 기본.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
+	int year = 2023;
 
 	// << 출력연산자
 	// endl : 개행(줄바꿈)
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello World "<< year << std::endl;
 
 	return 0;
 }
