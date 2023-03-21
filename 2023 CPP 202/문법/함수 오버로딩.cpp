4dcc802bc2adba3338cb911d7d33befb36dc8722
kye <kye6735@naver.com>
2023-03-21 14:41:18 +0900

    함수 오버로딩 하기 전

---
+++ 2023 CPP 202/문법/함수 오버로딩.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+float sum_float(float a, float b) {
+	return a + b;
+}
+
+int main(void) {
+	cout << sum(1, 2) << endl;
+	cout << sum_float(1.1f, 2.2f) << endl;
+
+	return 0;
+}
