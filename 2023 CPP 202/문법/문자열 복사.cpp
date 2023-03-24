2f4cd375c5a979bdb1e06ee292feffb57f86e2d8
kye <kye6735@naver.com>
2023-03-24 09:12:58 +0900

    C스타일 문자열 복사 strcpy()

--- 2023 CPP 202/문법/문자열 복사.cpp
+++ 2023 CPP 202/문법/문자열 복사.cpp
@@ -1,13 +1,13 @@
 ﻿#include <stdio.h>
-#include <string.h>
+#include <string.h>		// 문자열 함수가 들어있음
 
 int main(void) {
 	char str1[30] = "";
 	char str2[30] = "Yeeun";
 
-	for (int i = 0; str2[i] != '\0'; i++) {
-		str1[i] = str2[i];
-	}
+	// str1에 str2의 내용을 집어넣겠다
+	strcpy(str1, str2);
+
 	printf("%s \n", str1);
 
 	return 0;
