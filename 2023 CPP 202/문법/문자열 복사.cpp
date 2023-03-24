c91af901d9c3da59d2625a4299e4ca484f8d8fae
kye <kye6735@naver.com>
2023-03-24 09:09:02 +0900

    C스타일 문자열 복사

---
+++ 2023 CPP 202/문법/문자열 복사.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str1[30] = "";
+	char str2[30] = "Yeeun";
+
+	for (int i = 0; str2[i] != '\0'; i++) {
+		str1[i] = str2[i];
+	}
+	printf("%s \n", str1);
+
+	return 0;
+}
