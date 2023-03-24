d1264fb8737d3deb52ee7f0d996b58c7176f3b36
kye <kye6735@naver.com>
2023-03-24 09:46:35 +0900

    C스타일 문자열 길이 strlen()

--- 2023 CPP 202/문법/문자열 길이.cpp
+++ 2023 CPP 202/문법/문자열 길이.cpp
@@ -10,11 +10,8 @@ using namespace std;
 
 int main(void) {
 	char str1[30] = "Yeeun";
-	int len = 0;
+	int len = strlen(str1);
 
-	for (int i = 0; str1[i] != '\0'; i++) {
-		len++;
-	}
 	printf("문자열의 길이는 %d \n", len);
 
 	return 0; 
