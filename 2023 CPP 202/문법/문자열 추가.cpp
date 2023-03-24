ec9e86020b2a977defc9c68e421ef3f5e8128a62
kye <kye6735@naver.com>
2023-03-24 17:43:55 +0900

    C++스타일 문자열 추가

--- 2023 CPP 202/문법/문자열 추가.cpp
+++ 2023 CPP 202/문법/문자열 추가.cpp
@@ -9,13 +9,12 @@
 using namespace std;
 
 int main(void) {
-	char str1[30] = "Yeeun";
-	char str2[30] = "Rock";
+	string str1 = "Yeeun";
+	string str2 = "Rock";
 
-	// str2의 문자열을  str1(목적)에 추가
-	strcat(str1, str2);
+	str1 = str1 + str2;
 
-	printf("%s \n", str1);
+	cout << str1 << endl;
 
 	return 0;
 }
