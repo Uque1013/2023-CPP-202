0b82e82632a4bad2622e8173ed553b300736713e
kye <kye6735@naver.com>
2023-03-24 17:42:39 +0900

    C스타일 문자열 더하기

---
+++ 2023 CPP 202/문법/문자열 추가.cpp
@@ -0,0 +1,26 @@
+﻿// C
+#include <stdio.h>
+#include <string.h>
+
+// C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	char str1[30] = "Yeeun";
+	char str2[30] = "Rock";
+
+	const int s1_len = strlen(str1);
+	const int s2_len = strlen(str2);
+
+	for (int i = s1_len; i < s1_len + s2_len; i++) {
+		str1[i] = str2[i - s1_len];
+	}
+	str1[s1_len + s2_len] = '\0';
+
+	printf("%s \n", str1);
+
+	return 0;
+}
