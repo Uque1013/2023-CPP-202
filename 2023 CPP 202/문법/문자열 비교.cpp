b540c95a871d89cb07822f5f656e9e1d5a72f02e
kye <kye6735@naver.com>
2023-03-28 13:56:31 +0900

    C스타일 문자열비교 strcmp() 헷갈려

--- 2023 CPP 202/문법/문자열 비교.cpp
+++ 2023 CPP 202/문법/문자열 비교.cpp
@@ -7,39 +7,24 @@
 #include <string>
 
 using namespace std;
-// 시험에 안 나옴
+
 int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	const int s1_l = strlen(str1);
-	const int s2_l = strlen(str2);
-
-	// s1_l과 s2_l 중 최소값
-	const int l = s1_l < s2_l ? s1_l : s2_l;
-
-	int i = 0;
-	while (i < l) {
-		if (str1[i] > str2[i]) {
-			printf("str1 > str2");
-			break;
-		}
-		else if (str1[i] < str2[i]) {
-			printf("str1 < str2");
-			break;
-		}
-		i++;
+	// 문자열비교 (인자의 순서와 반환값이 헷갈림)
+	int result = strcmp(str1, str2);
+	
+	if (result == 0) {
+		printf("str1 == str2");
 	}
-	if (i == l) {
-		if (s1_l > s2_l) {
-			printf("str1 > str2");
-		}
-		else if (s1_l < s2_l) {
-			printf("str1 < str2");
-		}
-		else
-			printf("str1 == str2");
+	else if (result == -1) {
+		printf("str1 < str2");
 	}
+	else if (result == 1) {
+		printf("str1 > str2");
+	}
+
 
 	return 0;
 }
