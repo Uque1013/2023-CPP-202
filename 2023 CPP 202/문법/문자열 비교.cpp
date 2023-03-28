3d0791f80833de872f31e06c5c359bae9624ec4c
kye <kye6735@naver.com>
2023-03-28 13:51:24 +0900

    C스타일 문자열 비교

---
+++ 2023 CPP 202/문법/문자열 비교.cpp
@@ -0,0 +1,45 @@
+﻿// C
+#include <stdio.h>
+#include <string.h>
+
+// C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+// 시험에 안 나옴
+int main(void) {
+	char str1[50] = "Hello";
+	char str2[50] = "World";
+
+	const int s1_l = strlen(str1);
+	const int s2_l = strlen(str2);
+
+	// s1_l과 s2_l 중 최소값
+	const int l = s1_l < s2_l ? s1_l : s2_l;
+
+	int i = 0;
+	while (i < l) {
+		if (str1[i] > str2[i]) {
+			printf("str1 > str2");
+			break;
+		}
+		else if (str1[i] < str2[i]) {
+			printf("str1 < str2");
+			break;
+		}
+		i++;
+	}
+	if (i == l) {
+		if (s1_l > s2_l) {
+			printf("str1 > str2");
+		}
+		else if (s1_l < s2_l) {
+			printf("str1 < str2");
+		}
+		else
+			printf("str1 == str2");
+	}
+
+	return 0;
+}
