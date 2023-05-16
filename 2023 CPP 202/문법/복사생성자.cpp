0a62e02f6986a89e46b5e8c9bcead554b3dc04e0
kye <kye6735@naver.com>
2023-05-16 14:28:16 +0900

    복사생성자(얕은복사)

--- 2023 CPP 202/문법/복사생성자.cpp
+++ 2023 CPP 202/문법/복사생성자.cpp
@@ -13,6 +13,11 @@ public:
 		cout << "일반생성자 호출" << endl;
 	}
 
+	// 복사생성자(얕은복사) 오류발생
+	Munja(const Munja& hj ) : str_(hj.str_) {
+		cout << "복사생성자 호출" << endl;
+	}
+
 	~Munja() {
 		delete[] str_;
 		cout << "소멸자 호출" << endl;
@@ -23,8 +28,8 @@ private:
 };
 
 int main(void) {
-	Munja m1 = Munja("abc");
-
+	Munja m1 = Munja("abc");	 // 일반생성자 호출
+	Munja m2 = m1;						// 복사생성자 호출
 
 	return 0;
 }
