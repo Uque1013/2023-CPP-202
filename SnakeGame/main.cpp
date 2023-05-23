87a9135360f6800dcef4693521e61c1b6b188add
kye <kye6735@naver.com>
2023-05-23 13:58:38 +0900

    [SMFL] 사과 만들기

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -5,13 +5,20 @@ using namespace sf;
 int main(void) 
 {
 	RenderWindow window(VideoMode(640, 480), "Snake Game");
+
 	// 컴퓨터가 1초 동안 처리하는  횟수를 60으로 제한한다
+	// Frame Per Second를 60으로 조절
 	window.setFramerateLimit(60);
 
 	RectangleShape snake;
 	snake.setFillColor(Color::White);
 	snake.setPosition(100, 300);
 	snake.setSize(Vector2f(50, 50));
+
+	RectangleShape apple;
+	apple.setFillColor(Color::Red);
+	apple.setPosition(200, 400);
+	apple.setSize(Vector2f(50, 50));
 
 	while (window.isOpen())
 	{
@@ -35,6 +42,7 @@ int main(void)
 		window.clear();
 
 		window.draw(snake);
+		window.draw(apple);	// draw를 늦게 할 수록 더 위에 있다
 
 		window.display();
 	}
