cde0abc468fc96c8feef3e731e6c9b1470d2eee4
kye <kye6735@naver.com>
2023-05-23 14:50:33 +0900

    [SFML] 일부 데이터 값을 변수화

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -6,8 +6,12 @@ using namespace sf;
 
 int main(void) 
 {
+	const int WIDTH = 1000;
+	const int HEIGHT = 800;
+	const int BLOCK_SIZE = 50;
+
 	srand(time(NULL));
-	RenderWindow window(VideoMode(640, 480), "Snake Game");
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Snake Game");
 
 	// 컴퓨터가 1초 동안 처리하는  횟수를 60으로 제한한다
 	// Frame Per Second를 60으로 조절
@@ -16,13 +20,13 @@ int main(void)
 	RectangleShape snake;
 	snake.setFillColor(Color::White);
 	snake.setPosition(100, 300);
-	snake.setSize(Vector2f(50, 50));
+	snake.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 	RectangleShape apple;
 	apple.setFillColor(Color::Red);
 	// 640에 50을 뺀 이유는 사과가 화면 밖에 벗어나지 않게 하기 위함
-	apple.setPosition(rand() % (640 - 50), rand() % (480 - 50));
-	apple.setSize(Vector2f(50, 50));
+	apple.setPosition(rand() % (WIDTH - BLOCK_SIZE), rand() % (HEIGHT - BLOCK_SIZE));
+	apple.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 	while (window.isOpen())
 	{
@@ -46,7 +50,7 @@ int main(void)
 		
 		// 뱀이 사과를 먹었을 때,
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds())) {
-			apple.setPosition(rand() % (640 - 50), rand() % (480 - 50));
+			apple.setPosition(rand() % (WIDTH - BLOCK_SIZE), rand() % (HEIGHT - BLOCK_SIZE));
 		}
 
 		window.clear();
