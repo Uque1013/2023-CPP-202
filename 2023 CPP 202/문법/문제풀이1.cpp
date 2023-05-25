32f800183ad7a123de7146c4a3f353721dc9c34c
kye <kye6735@naver.com>
2023-05-26 08:57:20 +0900

    얕은복사 생성자

---
+++ 2023 CPP 202/문법/문제풀이1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Munja {
+public:
+	Munja(const char* jhqz)
+	{
+		// 3("abc") +1('\0')
+		str_ = new char[strlen(jhqz) + 1];
+		strcpy(str_, jhqz);	
+		cout << "일반생성자 호출" << endl;
+	}
+
+	// 복사생성자(깊은복사)
+	Munja(const Munja& hj ) : str_(hj.str_) {
+		str_ = new char[strlen(hj.str_) + 1];
+		strcpy(str_, hj.str_);
+		cout << "복사생성자 호출" << endl;
+	}
+
+	~Munja() {
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
+
+private:
+	char* str_;
+};
+
+int main(void) {
+	Munja m1 = Munja("abc");	 // 일반생성자 호출
+	Munja m2 = m1;						// 복사생성자 호출
+
+	return 0;
+}
