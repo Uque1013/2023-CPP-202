bf718d716c03c1274e91f58fcfcb210db2835cb2
kye <kye6735@naver.com>
2023-05-26 10:11:49 +0900

    [SFML] 클래스 설계 준비

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -9,6 +9,15 @@
 
 using namespace sf;
 
+class Snake {
+
+private:
+	int dir_;
+	int x_;
+	int y_;
+	RectangleShape sprite_;
+};
+
 int main(void) 
 {
 	const int WIDTH = 1000;													// 픽셀 너비
