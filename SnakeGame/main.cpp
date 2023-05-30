c2c429893e8354cdf4074002ae681ca9647c5b65
kye <kye6735@naver.com>
2023-05-30 14:39:09 +0900

    [SFML] 사과먹으면 뱀이 길어진다1

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -7,16 +7,24 @@
 #define DIR_RIGHT	2
 #define DIR_LEFT		3
 
+#define BODY_MAX	20
+
 using namespace sf;
+
+class Object {
+
+public:
+	int x_;
+	int y_;
+
+	RectangleShape sprite_;
+};
 
 class Snake {
 
 public:
 	int dir_;
-	int x_;
-	int y_;
-
-	RectangleShape sprite_;
+	Object body_[BODY_MAX];
 };
 
 class Apple {
@@ -44,12 +52,15 @@ int main(void)
 	window.setFramerateLimit(20);
 
 	Snake snake;
-	snake.x_ = 1, snake.y_ = 2;		// 뱀의 그리드 좌표 
 	snake.dir_ = DIR_DOWN;		// 뱀이 이동하는 방향
 
-	snake.sprite_.setFillColor(Color::White);
-	snake.sprite_.setPosition(BLOCK_SIZE * snake.x_, BLOCK_SIZE * snake.y_);
-	snake.sprite_.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
+	for (int i = 0; i < BODY_MAX; i++) {
+		snake.body_[i].x_ = -50, snake.body_[i].y_ = -50;		// 뱀의 그리드 좌표 
+		snake.body_[i].sprite_.setFillColor(Color::White);
+		snake.body_[i].sprite_.setPosition(BLOCK_SIZE * snake.body_[i].x_, BLOCK_SIZE * snake.body_[i].y_);
+		snake.body_[i].sprite_.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
+	}
+	snake.body_[0].x_ = 3, snake.body_[0].y_ = 3;
 
 	Apple apple;
 	apple.x_ = rand() % G_WIDTH, apple.y_ = rand() % G_HEIGHT;
@@ -84,22 +95,22 @@ int main(void)
 		}
 		
 		// update
-		if (snake.dir_ == DIR_UP && snake.y_ > 0) {
-			snake.y_--;
+		if (snake.dir_ == DIR_UP && snake.body_[0].y_ > 0) {
+			snake.body_[0].y_--;
 		}
-		else if (snake.dir_ == DIR_DOWN && snake.y_ < G_HEIGHT-1) {
-			snake.y_++;
+		else if (snake.dir_ == DIR_DOWN && snake.body_[0].y_ < G_HEIGHT-1) {
+			snake.body_[0].y_++;
 		}
-		else if (snake.dir_ == DIR_RIGHT && snake.x_ < G_WIDTH-1) {
-			snake.x_++;
+		else if (snake.dir_ == DIR_RIGHT && snake.body_[0].x_ < G_WIDTH-1) {
+			snake.body_[0].x_++;
 		}
-		else if (snake.dir_ == DIR_LEFT && snake.x_ > 0) {
-			snake.x_--;
+		else if (snake.dir_ == DIR_LEFT && snake.body_[0].x_ > 0) {
+			snake.body_[0].x_--;
 		}
-		snake.sprite_.setPosition(snake.x_ * BLOCK_SIZE, snake.y_ * BLOCK_SIZE);
+		snake.body_[0].sprite_.setPosition(snake.body_[0].x_ * BLOCK_SIZE, snake.body_[0].y_ * BLOCK_SIZE);
 
 		// 뱀이 사과를 먹었을 때,
-		if (snake.x_ == apple.x_&& snake.y_ == apple.y_) {
+		if (snake.body_[0].x_ == apple.x_&& snake.body_[0].y_ == apple.y_) {
 			apple.x_ = rand() % G_WIDTH, apple.y_ = rand() % G_HEIGHT;
 			apple.sprite_.setPosition(apple.x_ * BLOCK_SIZE, apple.y_ * BLOCK_SIZE);
 		}
@@ -107,7 +118,8 @@ int main(void)
 		// render
 		window.clear();
 
-		window.draw(snake.sprite_);
+		for (int i = 0; i < BODY_MAX; i++)
+			window.draw(snake.body_[i].sprite_);
 		window.draw(apple.sprite_);	// draw를 늦게 할 수록 더 위에 있다
 
 		window.display();
