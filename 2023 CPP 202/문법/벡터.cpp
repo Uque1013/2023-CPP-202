21591e7fa44e0f5cdb6173498c63fbeb2206efb8
kye <kye6735@naver.com>
2023-09-05 14:05:48 +0900

    전통적인 배열은 값을 넣기 불편하다

---
+++ 2023 CPP 202/문법/벡터.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) 
+{
+	int* arr = new int[4];
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 30;
+	arr[3] = 40;
+
+	// 꽉찬 공간에 새로운 데이터를 넣어보자
+	// 크기가 확장된 새로운 공간을 만든다.
+
+	int* temp = new int[6]; 
+	// 기존의 값을 복사한다. 
+
+	for (int i = 0; i < 4; i++)
+		temp[i] = arr[i];
+	// 기존 공간 삭제
+	delete[] arr;
+	// arr이 새로운 공간을 가리킴
+	arr = temp;
+	// 새로운 값을 넣어줌
+	arr[4] = 50;
+
+	for (int i = 0; i < 5; i++)
+		cout << arr[i] << " ";
+
+	return 0;
+}
