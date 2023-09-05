c6158f8d1b66d0b72f1357bf521f6eea081c8424
kye <kye6735@naver.com>
2023-09-05 14:33:18 +0900

    벡터는 크기가 자유롭게 변한다.

--- 2023 CPP 202/문법/벡터.cpp
+++ 2023 CPP 202/문법/벡터.cpp
@@ -1,29 +1,21 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void) 
 {
-	int* arr = new int[4];
-	arr[0] = 10;
-	arr[1] = 20;
-	arr[2] = 30;
-	arr[3] = 40;
+	// 전통적인 배열은 크기가 고정되어 있으나,
+	// 벡터는 크기가 자유롭게 변할 수 있는 동적배열임	. 
+	vector <int> arr;
+	arr.reserve(4); // 크기를 4로
+	arr.push_back(10);
+	arr.push_back(20);
+	arr.push_back(30);
+	arr.push_back(40);
 
 	// 꽉찬 공간에 새로운 데이터를 넣어보자
-	// 크기가 확장된 새로운 공간을 만든다.
-
-	int* temp = new int[6]; 
-	// 기존의 값을 복사한다. 
-
-	for (int i = 0; i < 4; i++)
-		temp[i] = arr[i];
-	// 기존 공간 삭제
-	delete[] arr;
-	// arr이 새로운 공간을 가리킴
-	arr = temp;
-	// 새로운 값을 넣어줌
-	arr[4] = 50;
+	arr.push_back(50);
 
 	for (int i = 0; i < 5; i++)
 		cout << arr[i] << " ";
