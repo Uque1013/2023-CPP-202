fa85aa020a3f31858dd885bc03c02766de92b89a
kye <kye6735@naver.com>
2023-10-17 14:31:44 +0900

    범위기반 for문

--- 2023 CPP 202/문법/범위기반 for.cpp
+++ 2023 CPP 202/문법/범위기반 for.cpp
@@ -1,24 +1,11 @@
 ﻿#include <iostream>
-#include <map>
-#include <string>
 
 using namespace std;
 
-int main(void) 
+int main(void)
 {
-	// key: 문자열, value: 정수
-	map<string, int> moving;
+	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
 
-	moving["번개맨"] = 1987;
-	moving["장희수"] = 2000;
-	moving["봉평"] = 1959;
-
-	cout << moving["장희수"] << endl;
-
-	map<string, int>::iterator iter;
-	// 맵은 순서가 없어서 iterator로만 반복문을 사용한다.
-	for (iter = moving.begin(); iter != moving.end(); iter++)
-		cout << iter->first << iter->second << " ";
-
-	return 0;
+	for (double x : arr)
+		cout << x << endl;
 }
