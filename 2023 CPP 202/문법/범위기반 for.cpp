beae8e620efad9dac37ce990d2bfdd57bffac62f
kye <kye6735@naver.com>
2023-10-17 14:44:36 +0900

    auto를 통하여 자료형으로 자동으로 맞춰준다

--- 2023 CPP 202/문법/범위기반 for.cpp
+++ 2023 CPP 202/문법/범위기반 for.cpp
@@ -6,6 +6,12 @@ int main(void)
 {
 	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
 
-	for (double x : arr)
+	// 누가봐도 a의 자료형은 int이다
+	auto a = 4;
+
+	// x의 자료형은 double로 자동설정
+	for (auto x : arr)
 		cout << x << endl;
+
+	return 0;
 }
