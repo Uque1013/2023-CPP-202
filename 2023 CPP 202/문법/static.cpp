fa814d0b200083a3e79fabf583a3ca8a0d6d9244
kye <kye6735@naver.com>
2023-11-14 13:36:24 +0900

    static 멤버변수

---
+++ 2023 CPP 202/문법/static.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Champ {
+public:
+	Champ(const string& name);
+private:
+	static int cnt_;
+};
+
+// static 멤버변수는 무조건 바깥에서 정의한다(const 같은거 제외)
+int Champ::cnt_ = 0;
+
+// 바깥에서 멤버를 정의할 때는 멤버변수나 멤버함수 이름앞에 클래스이름::
+Champ::Champ(const string& name) {
+	++cnt_;
+}
+
+int main(void)
+{
+	Champ* a = new Champ("오리아나");
+	Champ* b = new Champ("아지르");
+
+	return 0;
+}
